58730de0214c522572292e0667fa0c7c2d73a5e9
Eugen <eugen.ovchinnikova@gmail.com>
2017-09-24 17:47:27 +0300

    Collections

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task33.task3308;
+
+import java.util.List;
+
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+    static class Goods{
+        public List<String> names;
+    }
+}
