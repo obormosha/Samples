baa0823ad343bd3b2bd516a6921e01e3a821d908
Eugen <eugen.ovchinnikova@gmail.com>
2017-10-01 15:18:47 +0300

    Collections

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/FactoryProducer.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task37.task3702;
+
+import com.javarush.task.task37.task3702.female.FemaleFactory;
+import com.javarush.task.task37.task3702.male.MaleFactory;
+
+public class FactoryProducer {
+
+    public static enum HumanFactoryType {
+        MALE,
+        FEMALE
+    }
+
+    public static AbstractFactory getFactory(HumanFactoryType type) {
+        AbstractFactory factory = null;
+        switch (type) {
+            case MALE:
+                factory = new MaleFactory();
+                break;
+            case FEMALE:
+                factory = new FemaleFactory();
+        }
+        return factory;
+    }
+}
